22adb6c2f1f2d1d4ee3e731fb89cc14f2a022d4d
Alexander Robinson <robinson@fis.ucm.es>
2020-02-29 18:31:41 +0100

    rembo.x runs, though topography is not loaded.

--- src/standalone_main.f90
+++ src/standalone_main.f90
@@ -1,35 +1,47 @@
 program emb_driver
   
-  use emb_global
-  
-  implicit none
+    use rembo_sclimate 
+    
+    implicit none
 
-  integer :: n_step
-  double precision :: timer_start, timer_tot
+    integer :: n_step, n_step_max 
+    double precision :: timer_start, timer_tot
+    real(8) :: year0
+    real(8) :: yearf 
+    real(8) :: T_summer 
 
-  write(*,*)
-  write(*,*) "  *** 2D Diffusion: Temperature and precipitation ***"
-  write(*,*) "                   over Greenland" 
-  write(*,*)
-      
-  ! Get start time in seconds
-  call cpu_time(timer_start) 
-  
-  ! Initialize the model
-  call sclimate(0)
-  call timing(0,timer_start,timer_tot)
-  
-  nstep_max = yearf - year0
-  
-  ! ### Run iterations ###
-  do n_step = 1, nstep_max    ! in years
+    write(*,*)
+    write(*,*) "  *** 2D Diffusion: Temperature and precipitation ***"
+    write(*,*) "                   over Greenland" 
+    write(*,*)
+    
+    T_summer = 0.0 
 
-    call sclimate(n_step)
+    year0 = 0.0 
+    yearf = 10.0 
 
-    ! Update the timers for each timestep and output
-    call timing(n_step,timer_start,timer_tot)
+    ! Get start time in seconds
+    call cpu_time(timer_start) 
   
-  end do 
+    ! Initialize the climate model REMBO, including loading parameters from options_rembo 
+    call rembo_init()
+    call timing(0,timer_start,timer_tot)
+
+    ! Update REMBO, with ice sheet topography    
+    call rembo_update(0,T_summer)
+     
+    n_step_max = yearf - year0
+  
+    ! ### Run iterations ###
+    do n_step = 1, n_step_max    ! in years
+
+        ! call REMBO1     
+        call rembo_update(n_step,T_summer)
+            
+        ! Update the timers for each timestep and output
+        call timing(n_step,timer_start,timer_tot)
+
+    end do 
 
   
 end program emb_driver
