55a46e72f088489c9fe1656979eca8fc98b94b4c
Alexander Robinson <alexander.robinson@awi.de>
2026-06-26 11:59:45 +0200

    build: rely on fesm-utils for shared modules; drop vendored copies
    Remove libs/{nml,ncio,ncio_transpose,interp1D,gaussian_filter}.f90 and wire
    the build to fesm-utils (libfesmutils): add FESMUTILSROOT/INC/LIB to
    common.mk (listed before coordinates so its modules win), drop the local
    build rules and the ncio/gaussian_filter prereqs. interp1D now resolves to
    fesm-utils' dp-capable version. rembo source is unchanged (same module
    names). Requires a fesm-utils symlink (as for the other models).

--- config/Makefile_rembo.mk
+++ config/Makefile_rembo.mk
@@ -7,13 +7,13 @@ $(objdir)/exchange.o : $(srcdir)/exchange.f90 $(objdir)/parameters.o
 $(objdir)/parameters.o : $(srcdir)/parameters.f90
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 
-$(objdir)/emb_global.o : $(srcdir)/emb_global.f90 $(objdir)/ncio.o $(objdir)/exchange.o
-	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $< 
+$(objdir)/emb_global.o : $(srcdir)/emb_global.f90 $(objdir)/exchange.o
+	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 $(objdir)/emb_pdesolver.o : $(srcdir)/emb_pdesolver.f90 $(objdir)/emb_global.o 
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 $(objdir)/projector.o : $(srcdir)/projector.f90 $(objdir)/emb_global.o 
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
-$(objdir)/emb_functions.o : $(srcdir)/emb_functions.f90 $(objdir)/emb_global.o $(objdir)/ncio.o
+$(objdir)/emb_functions.o : $(srcdir)/emb_functions.f90 $(objdir)/emb_global.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 $(objdir)/rembo_functions.o : $(srcdir)/rembo_functions.f90 $(objdir)/emb_global.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
@@ -23,7 +23,7 @@ $(objdir)/rembo_main.o : $(srcdir)/rembo_main.f90 $(objdir)/rembo_functions.o $(
 $(objdir)/emb_functions.o $(objdir)/emb_pdesolver.o $(objdir)/projector.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 $(objdir)/climate.o : $(srcdir)/climate.f90 $(objdir)/emb_global.o $(objdir)/emb_functions.o \
- 							$(objdir)/rembo_main.o $(objdir)/gaussian_filter.o  
+ 							$(objdir)/rembo_main.o
 	$(FC) $(DFLAGS) $(FFLAGS) -c -o $@ $<
 
 $(objdir)/sinsol_orbit.o : $(srcdir)/sinsol_orbit.f
--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,14 @@
 # Loaded after the compiler and machine fragments. References FFLAGS /
 # FFLAGS_OPENMP (compiler) and LIB_NC (machine or auto-detected netCDF).
 
+# fesm-utils provides the shared utility modules (ncio, nml, interp1D,
+# gaussian_filter, ncio_transpose). rembo1 relies on these directly instead of
+# vendoring its own copies. Listed before coordinates so its modules win on the
+# include search path where names overlap.
+FESMUTILSROOT = fesm-utils/utils
+INC_FESMUTILS = -I${FESMUTILSROOT}/include-serial
+LIB_FESMUTILS = -L${FESMUTILSROOT}/include-serial -lfesmutils
+
 # coordinates is linked into libs/coordinates (see this package's
 # [[package.links]] in configme: rembo1 -> coordinates at libs/coordinates).
 COORDROOT = libs/coordinates
@@ -13,10 +21,15 @@ LISROOT = fesm-utils/lis-serial
 INC_LIS = -I${LISROOT}/include
 LIB_LIS = -L${LISROOT}/lib/ -llis
 
-# rembo1 needs the coordinates include in its compile flags.
+# rembo1 needs the fesm-utils and coordinates includes in its compile flags
+# (fesm-utils first so its modules take precedence over coordinates' copies).
+FFLAGS += $(INC_FESMUTILS)
 FFLAGS += $(INC_COORD)
 
 ifeq ($(openmp), 1)
+    INC_FESMUTILS = -I${FESMUTILSROOT}/include-omp
+    LIB_FESMUTILS = -L${FESMUTILSROOT}/include-omp -lfesmutils
+
     LISROOT = fesm-utils/lis-omp
     INC_LIS = -I${LISROOT}/include
     LIB_LIS = -L${LISROOT}/lib/ -llis
@@ -30,4 +43,4 @@ INC_REMBO = -I${REMBOROOT}/librembo/include
 LIB_REMBO = -L${REMBOROOT}/librembo/include -lrembo
 
 LFLAGS_EXTRA ?= -Wl,-zmuldefs
-LFLAGS = $(LIB_NC) $(LIB_COORD) $(LIB_LIS) $(LFLAGS_EXTRA)
+LFLAGS = $(LIB_NC) $(LIB_FESMUTILS) $(LIB_COORD) $(LIB_LIS) $(LFLAGS_EXTRA)
